eb22f8a8ab74da56b81f465e8d15ced646ab0ba2
raniaderouiche <rania.derouiche@gmail.com>
2024-10-03 16:17:07 +0100

    video highlights

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Event } from '../models/Event';
 import { HttpClient } from '@angular/common/http';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-events',
@@ -15,15 +16,29 @@ export class EventsComponent implements OnInit {
   list_items: any = []
   selected_event: Event = {}
 
-  constructor(private http: HttpClient) { }
+  videos_url: string = '/assets/data/videos.json';
+  videos:any = [];
+
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.getEvents()
+    this.getVideos()
+  }
+
+  getSafeUrl(url: string): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
   getEvents(){
     this.http.get(this.url).subscribe(res => {
       this.list_items = res;
+    });
+  }
+
+  getVideos(){
+    this.http.get(this.videos_url).subscribe(res => {
+      this.videos = res;
     });
   }
 
@@ -43,7 +58,9 @@ export class EventsComponent implements OnInit {
   }
 
   onNext(){
-    this.current_page = this.current_page + 1
+    if (this.current_page < Math.ceil(this.list_items.length / this.rows)) {
+      this.current_page = this.current_page + 1
+    }
   }
 
   onPrevious(){
@@ -54,5 +71,35 @@ export class EventsComponent implements OnInit {
 
   OpenModal(event:any){
     this.selected_event = event
+  }
+
+  videos_currentPage = 1;
+  pageSize = 2; // Number of videos per page
+
+  nextPage() {
+    if (this.videos_currentPage < Math.ceil(this.videos.length / this.pageSize)) {
+      this.videos_currentPage++;
+    }
+  }
+
+  prevPage() {
+    if (this.videos_currentPage > 1) {
+      this.videos_currentPage--;
+    }
+  }
+
+  paginatedVideos(page: number){
+    const start = this.pageSize * (page - 1)
+    const end = start + this.pageSize
+    return this.videos.slice(start, end)
+  }
+
+  setupVideosPagination(){
+    const page_count = Math.ceil(this.videos.length / this.pageSize)
+    return Array.from({length: page_count}, (_,i) => i + 1)
+  }
+
+  onVideosPageChange(page: number){
+    this.videos_currentPage = page
   }
 }
